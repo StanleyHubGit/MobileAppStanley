51946bc081868e09619af5c18e095da395561422
Stanley <limstanley05@gmail.com>
2025-09-02 15:48:15 +0700

    Latihan API

--- app/src/main/java/edu/uph/m23si2/pertamaapp/LoginActivity.java
+++ app/src/main/java/edu/uph/m23si2/pertamaapp/LoginActivity.java
@@ -2,9 +2,13 @@ package edu.uph.m23si2.pertamaapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,18 +17,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uph.m23si2.pertamaapp.api.ApiResponse;
+import edu.uph.m23si2.pertamaapp.api.ApiResponseKabupaten;
+import edu.uph.m23si2.pertamaapp.api.ApiService;
 import edu.uph.m23si2.pertamaapp.model.KRS;
 import edu.uph.m23si2.pertamaapp.model.KRSDetail;
+import edu.uph.m23si2.pertamaapp.model.Kabupaten;
 import edu.uph.m23si2.pertamaapp.model.KelasMatakuliah;
 import edu.uph.m23si2.pertamaapp.model.Mahasiswa;
 import edu.uph.m23si2.pertamaapp.model.Matakuliah;
 import edu.uph.m23si2.pertamaapp.model.Prodi;
+import edu.uph.m23si2.pertamaapp.model.Provinsi;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     EditText edtNama, edtPassword;
+    Spinner sprProvinsi, sprKabupaten;
+    List<Provinsi> provinsiList =  new ArrayList<>();
+    List<String> namaProvinsi = new ArrayList<>();
+    List<Kabupaten> kabupatenList = new ArrayList<>();
+    List<String> namaKabupaten = new ArrayList<>();
+    ArrayAdapter<String> adapter, adapterKabupaten;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +68,95 @@ public class LoginActivity extends AppCompatActivity {
                 .deleteRealmIfMigrationNeeded()
                 .build();
         Realm.setDefaultConfiguration(config);
-        initData();
+        //initData();
+        sprProvinsi = findViewById(R.id.sprProvinsi);
+        adapter =new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,namaProvinsi);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        sprProvinsi.setAdapter(adapter);
+
+        sprKabupaten = findViewById(R.id.sprKabupaten);
+        adapterKabupaten = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, namaKabupaten);
+        adapterKabupaten.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        sprKabupaten.setAdapter(adapterKabupaten);
+
+        //init retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://wilayah.id")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        //panggil API
+        apiService.getProvinsi().enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                if(response.isSuccessful() && response.body()!=null){
+                    provinsiList = response.body().getData();
+                    namaProvinsi.clear();
+                    for(Provinsi p: provinsiList){
+                        if(p.getName()!=null){
+                            Log.d("Provinsi", p.getName());
+                            namaProvinsi.add(p.getName());
+                        }
+                    }
+
+                    adapter.notifyDataSetChanged();
+
+                    sprProvinsi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Provinsi selectedProvinsi = provinsiList.get(position);
+                            Log.d("Provinsi", selectedProvinsi.getCode() + " - " + selectedProvinsi.getName());
+
+                            // Panggil API kabupaten sesuai kode provinsi
+                            apiService.getKabupaten(selectedProvinsi.getCode()).enqueue(new Callback<ApiResponseKabupaten>() {
+                                @Override
+                                public void onResponse(Call<ApiResponseKabupaten> call, Response<ApiResponseKabupaten> response) {
+                                    if(response.isSuccessful() && response.body()!=null){
+                                        kabupatenList = response.body().getData();
+                                        namaKabupaten.clear();
+                                        for(Kabupaten k : kabupatenList){
+                                            if(k.getName()!=null){
+                                                Log.d("Kabupaten", k.getName());
+                                                namaKabupaten.add(k.getName());
+                                            }
+                                        }
+                                        adapterKabupaten.notifyDataSetChanged();
+
+                                        sprKabupaten.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                            @Override
+                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                Kabupaten selectedKabupaten = kabupatenList.get(position);
+                                                Log.d("Kabupaten", selectedKabupaten.getCode() + " - " + selectedKabupaten.getName());
+                                            }
+
+                                            @Override
+                                            public void onNothingSelected(AdapterView<?> parent) { }
+                                        });
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<ApiResponseKabupaten> call, Throwable t) {
+                                    Toast.makeText(LoginActivity.this,"Gagal : "+t.getMessage(),Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                        }
+                    });
+                }
+            }
+
+                    @Override
+            public void onFailure(Call<ApiResponse> call, Throwable t) {
+                Toast.makeText(LoginActivity.this,"Gagal :"+t.getMessage(),Toast.LENGTH_LONG);
+            }
+        });
+
+
         btnLogin = findViewById(R.id.btnLogin);
         edtNama = findViewById(R.id.edtNama);
         edtPassword = findViewById(R.id.edtPassword);
@@ -56,10 +167,12 @@ public class LoginActivity extends AppCompatActivity {
                 toDashboard();
             }
         });
+
+
+
     }
 
-    public void initData(){
-        // menambahkan data
+    public void initData(){ // menambahkan data prodi dan matakuliah
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(r -> {
             Number maxId = r.where(Mahasiswa.class).max("studentID");
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.12.2")
         classpath("io.realm:realm-gradle-plugin:10.15.1")
     }
 }
